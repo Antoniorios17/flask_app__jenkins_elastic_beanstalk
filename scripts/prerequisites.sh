c50834ec640802b8e095ba039bb76aa44b61b05c
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-09-15 23:54:26 -0400

    updated script - 15-09-2023

---
+++ scripts/prerequisites.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+# Install pre-requisites
+
+# Install python virtual environment
+sudo apt install python3.10-venv -y
+
+# Install pip | python package manager
+sudo apt install python3-pip -y
+
+# Install unzip
+sudo apt install unzip
